767c825068d5f750b2c8b6fd437556f78396695d
jamiiecb <jamiiecb@googlemail.com>
2008-09-03 15:29:29 +0000

    Pen now works
    darcs-hash:20080903152929-b18dd-021fba476c0ef08cb4e829c8a73c8096d123f8ad.gz

--- src/pen/pen.clj
+++ src/pen/pen.clj
@@ -1,3 +1,33 @@
 (ns pen
-  (:uses utils qt-repl canvas))
+  (:uses utils canvas cell)
+  (:imports (com.trolltech.qt.gui QGraphicsPathItem QPainterPath)))
 
+(defcell path [] nil)
+(defcell path-item [path] nil)
+
+(defn pen-down [pos]
+  (dosync 
+    (let [p (QPainterPath. pos)
+          i (QGraphicsPathItem.)]
+      (. scene (addItem i))
+      (cell-set path p)
+      (cell-set path-item
+        (doto i (setPath (decell path)))))))
+
+(defn pen-move [pos]
+  (dosync
+    (. (decell path) (lineTo pos))
+    (modified path)))
+
+(defn pen-up [pos]
+  (dosync
+    (cell-set path-item nil)
+    (cell-set path nil)))
+
+(def pen-tool
+  {:press pen-down
+   :2press pen-down
+   :release pen-up
+   :move pen-move})
+   
+(dosync (alter tools #(assoc % lbutton pen-tool)))
